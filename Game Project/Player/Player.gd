c5e9674d06bd92671ade2109d0ff3527b1bf7b40
Elad Karni <elad.karni@gmail.com>
2020-02-01 15:32:18 -0500

    Movement Phase 2

--- Game Project/Player/Player.gd
+++ Game Project/Player/Player.gd
@@ -1,25 +1,30 @@
 extends KinematicBody2D
 
-export var speed: = Vector2(300, 500)
-export var gravity: = 2000.0
-var velocity: = Vector2.ZERO
-
 onready var player_sprite = get_node("Sprite")
 onready var player_anim = get_node("AnimationPlayer")
+onready var dash_timer = get_node("Dash_Timer")
+onready var jump_timer = get_node("Jump_Timer")
 
+export var MAX_SPEED: = Vector2(275, 500)
+export var MOVE_SPEED := Vector2(275, 500)
+export var DASH := 1000
+export var GRAVITY := 2000.0
 export var MAX_JUMPS := 1
+
+var VELOCITY: = Vector2.ZERO
+
 var CURRENT_JUMP := 0
 
 func _physics_process(delta: float) -> void:
 	var direction: = get_direction()
-	velocity = calculate_move_velocity(velocity, direction, speed)
-	velocity = move_and_slide(velocity, Vector2(0, -1)) 
+	VELOCITY = calculate_move_velocity(VELOCITY, direction, MAX_SPEED, MOVE_SPEED)
+	VELOCITY = move_and_slide(VELOCITY, Vector2(0, -1)) 
 
-	if Input.is_action_just_pressed("dash"):
+	if Input.is_action_just_pressed("ui_dash"):
 		dash()
 	
-	
 	# Animation
+	"""
 	var anim_idle = "Idle"
 	var anim_walk = "Walk"
 	
@@ -31,6 +36,7 @@ func _physics_process(delta: float) -> void:
 		player_sprite.set_flip_h(false)
 	elif velocity.x < 0:
 		player_sprite.set_flip_h(true)
+	"""
 	
 func get_direction() -> Vector2:
 	return Vector2(
@@ -50,18 +56,23 @@ func player_jump():
 func calculate_move_velocity(
 		linear_velocity: Vector2,
 		direction: Vector2,
+		max_speed: Vector2,
 		speed: Vector2
 	 ) -> Vector2:
 	var new_velocity: = linear_velocity
-	new_velocity.x = speed.x * direction.x
-	new_velocity.y += gravity * get_physics_process_delta_time()
+	new_velocity.x = max_speed.x * direction.x
+	new_velocity.y += GRAVITY * get_physics_process_delta_time()
 	if direction.y == -1.0:
-		new_velocity.y = speed.y * direction.y
+		new_velocity.y = max_speed.y * direction.y
 	return new_velocity
 
 func dash():
-	speed.x = 1000
-	$Timer.start()
+	MAX_SPEED.x = DASH
+	dash_timer.start()
 
-func _on_Timer_timeout() -> void:
-	speed.x = 100
+func _on_Dash_Timer_timeout() -> void:
+	MAX_SPEED.x = 275
+
+
+func _on_Jump_Timer_timeout() -> void:
+	pass # Replace with function body.
