0a9a09c267bd8efc1e4aba343a58e507be928a8b
Elad Karni <elad.karni@gmail.com>
2020-02-02 09:15:21 -0500

    Moved around player HUD

--- Game Project/Scripts/AutoRun.gd
+++ Game Project/Scripts/AutoRun.gd
@@ -1,11 +1,11 @@
 extends Node
 
-var first_run := false
-var portal_broken := true
-var jump_upgrade := true
-var dash_upgrade := true
+var first_run := true
+var portal_broken := false
+var jump_upgrade := false
+var dash_upgrade := false
 var has_parts := false
-var max_jump_count := 2
+var max_jump_count := 1
 
 func get_parts() -> void:
 	has_parts = true
